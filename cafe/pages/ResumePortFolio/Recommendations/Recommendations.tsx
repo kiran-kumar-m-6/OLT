79fedfc503a1d7581dd4e4db5de4f377075e6ac9
Kiran Kumar M <kirankumar.mattupalli@softsuave.org>
2026-03-05 19:27:25 +0530

    commit

--- cafe/pages/ResumePortFolio/Recommendations/Recommendations.tsx
+++ cafe/pages/ResumePortFolio/Recommendations/Recommendations.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "@/components/atom";
-import { RecommendationsType } from "@/Data/Type/PortFolio/Recommendations";
+import { RecommendationsType } from "@/Data/Type/PortFolio/PortFolioTypes"; 
 
 type Props = {
   data: RecommendationsType[];
@@ -9,16 +9,15 @@ export default function Recommendations({ data }: Props) {
   return (
     <div className="pt-[70px]" id="target-education">
       <div className="text-center grid justify-center">
-        <h2
-          className="font-bold capitalize text-[#2B2B2B] dark:text-white text-lg sm:text-3xl md:text-4xl lg:text-5xl" >
+        <h2 className="font-bold capitalize text-[#2B2B2B] dark:text-white text-lg sm:text-3xl md:text-4xl lg:text-5xl">
           recommendations
         </h2>
-
-        <p className="text-[#767676] dark:text-gray-400 text-center pt-4 max-w-[438px] capitalize" >
+        <p className="text-[#767676] dark:text-gray-400 text-center pt-4 max-w-[438px] capitalize">
           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
           sint. Velit officia consequat duis enim velit mollit. Lorem ipsum
         </p>
       </div>
+
       <div className="pt-[50px] flex justify-center flex-wrap gap-5">
         {data.map((review, index) => (
           <div
@@ -46,13 +45,13 @@ export default function Recommendations({ data }: Props) {
 
               <div className="grid justify-center align-middle sm:justify-center md:flex items-center gap-[13px]">
                 <div className="flex justify-center">
-                <Icon
-                  src={review.reviewerImage ?? ""}
-                  alt={review.reviewerName ?? "Reviewer Image"}
-                  width={80}
-                  height={80}
-                  className="object-scale-down shrink-0"
-                />
+                  <Icon
+                    src={review.reviewerImage ?? ""}
+                    alt={review.reviewerName ?? "Reviewer Image"}
+                    width={80}
+                    height={80}
+                    className="object-scale-down shrink-0"
+                  />
                 </div>
                 <div className="flex flex-col justify-center leading-tight">
                   <h4 className="font-semibold text-center md:text-left text-[#2B2B2B] dark:text-white m-0">
@@ -68,6 +67,7 @@ export default function Recommendations({ data }: Props) {
           </div>
         ))}
       </div>
+
       <div className="flex justify-center pt-[50px]">
         <Icon src="/Group.png" alt="End" width={50} height={10} />
       </div>
